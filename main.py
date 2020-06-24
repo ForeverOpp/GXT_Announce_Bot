26768024acb8a63b12383bca8580599795af5af9
ForeverOpp <life.app.hanfeng@hotmail.com>
2020-06-24 12:55:18 +0800

    小工具完成

---
+++ main.py
@@ -0,0 +1,32 @@
+#/html/body/form/div[3]/div[4]/div[2]/div/table/tbody/tr[2]/td[3]
+import requests
+from lxml import etree
+from MessageSender import MessageSender
+import time
+
+__header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36',
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
+__domain = "http://gxt.nmg.gov.cn/Xinxigongkai/GongkaiMulu/"
+__index = "newGongkaiMulu.aspx?e=tz"
+__sleepTime = 10 * 60
+m = MessageSender("Bark")
+m.config({'apikey': "gpKSL4RQYEZyTiKyz9vtEe", 'linkMode': True})
+announce = {'title': ""}
+while True:
+    try:
+        html = requests.get(__domain + __index).text
+        data = etree.HTML(html)
+        title = str(data.xpath('//*[@id="GVData"]/tr[2]/td[3]/a//text()')[0])
+        t = str(data.xpath('//*[@id="GVData"]/tr[2]/td[4]/span/text()')[0])
+        link = __domain + str(data.xpath('//*[@id="GVData"]/tr[2]/td[3]/a/@href')[0])
+    except:
+        continue
+    else:
+        tmpAnnounce = {'title': title, 'content': "发布日期：" + t + "，点击查看", 'link': link}
+        if tmpAnnounce['title'] == announce['title']:
+            time.sleep(__sleepTime)
+            continue
+        announce = tmpAnnounce.copy()
+        m.send(announce)
+        time.sleep(__sleepTime)
